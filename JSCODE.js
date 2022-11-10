9112576e41f3359319df746a019c65c4022a368b
sahnCord <insahn.cordeiro@yahoo.com>
2022-11-10 16:04:22 -0300

    - Improved and orginazed JS code; - Add Event Listenners; - Urgency buttons ok

--- JSCODE.js
+++ JSCODE.js
@@ -2,61 +2,62 @@ const shopDate = document.getElementById('date-shopping');
 const shopPlace = String(document.getElementById('local-shopping'));
 
 let shopItems; // Create a function to get all items, pricces and amount.
-determineUrgencyLevel
 
-let Ubuttons;
+//-------------------------------------------------------------------------
 
-function determineUrgencyLevel() {
-  
-  let But1 = document.getElementById(`Level1`)
-  let But2 = document.getElementById(`Level2`)
-  let But3 = document.getElementById(`Level3`)
-  let ButSet = [But1, But2, But3]
-  
-  if (But1 == true) {
-    const CButton = ButSet.splice(ButSet.indexOf(But1), 1);
-    CButton.setAtribute(`checked`);
-    
-    ButSet.forEach((u) => {
-      u.removeAtribute(`checked`);
-    });
-    let body = document.getElementByName(`body`)
-    body.style.background-color = red;
-  }
-  if (But2 == true) {
-    const CButton = ButSet.splice(ButSet.indexOf(But2), 1);
-    CButton.setAtribute(`checked`);
-    
-    ButSet.forEach((u) => {
-      u.removeAtribute(`checked`);
-    });
-  }
-  if (But3 == true) {
-    const CButton = ButSet.splice(ButSet.indexOf(But3), 1);
-    CButton.setAtribute(`checked`);
-    
-    ButSet.forEach((u) => {
-      u.removeAtribute(`checked`);
-    });
-  }
-}
+const buttonsSection = document.querySelector(".buttons-section");
+let determineUrgencyLevel = function () {
 
-let Ulevel;
+    let But1 = document.getElementById(`Level1`);
+    let But2 = document.getElementById(`Level2`);
+    let But3 = document.getElementById(`Level3`);
 
-let receivePlace;
+    let choseBox = null
 
-class insertShoppingData {
-    constructor() {};
-
-    createShoppingListobj = function (date, place, items, Ulevel, receivep) {
-        let OBJ = {
-            DateShop: date,
-            PlaceShop: place,
-            ReceivePlace: receivep
-        };
-        
-        return OBJ
+    if (But1.checked) {
+        But2.checked = false
+        But3.checked = false
+        choseBox = But1
     }
+    if (But2.checked) {
+        But1.checked = false
+        But3.checked = false
+        choseBox = But2
+    }
+    if (But3.checked) {
+        But1.checked = false
+        But2.checked = false
+        choseBox = But3
+    }
+
+    if (choseBox.value) return choseBox.value
+    
 }
 
-const act = new insertShoppingData
+buttonsSection.addEventListener("click", determineUrgencyLevel)
+let Ulevel = determineUrgencyLevel();
+
+//-------------------------------------------------------------------------
+
+const receivePlaceCheck = document.querySelector(".receive-place-check");
+const determineReceivePlace = function () {
+
+    let Home = document.getElementById("Home");
+    let Local = document.getElementById("Local");
+    let choseBox = null;
+
+    if (Home.checked) {
+        Local.checked = false
+        choseBox = Home
+    }
+    if (Local.checked) {
+        Home.checked = false
+        choseBox = Local
+    }
+
+    console.log(choseBox.value)
+
+    if (choseBox.value) return choseBox.value
+}
+receivePlaceCheck.addEventListener('click', determineReceivePlace);
+let receivePlace = receivePlaceCheck();
